74e4e95d3c65c27015ce1b33304f37438b2894d5
Matthew Richardson <M.Richardson@ed.ac.uk>
2024-12-26 15:54:50 +0000

    Autologin on tty1

--- install-vncviewer.sh
+++ install-vncviewer.sh
@@ -2,11 +2,20 @@
 
 # Run vncviewer in a GUI for connecting to: https://github.com/MindFreeze/home-assistant-addons
 
+# Enable autologin on tty1
+sudo mkdir -p /etc/systemd/system/getty@tty1.service.d/
+cat << 'EOF' > /etc/systemd/system/getty@tty1.service.d/autologin.conf
+[Service]
+ExecStart=
+ExecStart=-/sbin/agetty --autologin pi --noclear %I $TERM
+EOF
+
 # Install deps
 sudo apt-get install --no-install-recommends -y cage xwayland libgl1-mesa-dri libwayland-egl1-mesa mesa-vulkan-drivers tigervnc-viewer tigervnc-tools
 
-cat << 'EOF' >> /tmp/foo # /home/pi/.profile
-[ "$(tty)" == "/dev/tty1" ] && exec cage "vncviewer -passwd /home/pi/.vnc/passwd homeassistant.local::5901"
+cat << 'EOF' >> /home/pi/.profile
+export XKB_DEFAULT_LAYOUT=gb
+[ "$(tty)" == "/dev/tty1" ] && exec cage -s -- vncviewer -passwd /home/pi/.vnc/passwd homeassistant.local::5901
 EOF
 
 echo "Now set the vnc pasword for your HA server by running: vncpasswd\n then reboot."
