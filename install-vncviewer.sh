6e22ed0d429f357c8ff5988643c6a2ccbcf61902
Matthew Richardson <M.Richardson@ed.ac.uk>
2024-12-26 15:06:16 +0000

    Add wayland deps. Quote EOF to prevent variable expansion.

--- install-vncviewer.sh
+++ install-vncviewer.sh
@@ -3,10 +3,10 @@
 # Run vncviewer in a GUI for connecting to: https://github.com/MindFreeze/home-assistant-addons
 
 # Install deps
-sudo apt-get install --no-install-recommends -y cage tigervnc-viewer tigervnc-tools
+sudo apt-get install --no-install-recommends -y cage xwayland libgl1-mesa-dri libwayland-egl1-mesa mesa-vulkan-drivers tigervnc-viewer tigervnc-tools
 
-cat <<EOF >> /home/pi/.profile
-[ "$(tty)" = "/dev/tty1" ] && exec cage "vncviewer -passwd /home/pi/.vnc/passwd homeassistant.local::5901"
+cat << 'EOF' >> /tmp/foo # /home/pi/.profile
+[ "$(tty)" == "/dev/tty1" ] && exec cage "vncviewer -passwd /home/pi/.vnc/passwd homeassistant.local::5901"
 EOF
 
 echo "Now set the vnc pasword for your HA server by running: vncpasswd\n then reboot."
